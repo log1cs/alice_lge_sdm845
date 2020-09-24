4d738de5fb72fc0a1469cdba389650577e633c61
Al Viro <viro@zeniv.linux.org.uk>
2020-09-24 19:41:58 -0400

    ep_create_wakeup_source(): dentry name can change under you...
    commit 3701cb59d892b88d569427586f01491552f377b1 upstream.
    
    or get freed, for that matter, if it's a long (separately stored)
    name.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/eventpoll.c
+++ fs/eventpoll.c
@@ -1261,7 +1261,7 @@ static int reverse_path_check(void)
 
 static int ep_create_wakeup_source(struct epitem *epi)
 {
-	const char *name;
+	struct name_snapshot n;
 	struct wakeup_source *ws;
 #ifdef CONFIG_LGE_PM
 	char buf[40] = {0,};
@@ -1283,9 +1283,10 @@ static int ep_create_wakeup_source(struct epitem *epi)
 		if (!epi->ep->ws)
 			return -ENOMEM;
 	}
-#endif
-	name = epi->ffd.file->f_path.dentry->d_name.name;
-	ws = wakeup_source_register(name);
+
+	take_dentry_name_snapshot(&n, epi->ffd.file->f_path.dentry);
+	ws = wakeup_source_register(n.name);
+	release_dentry_name_snapshot(&n);
 
 	if (!ws)
 		return -ENOMEM;
