53f933c8a303a5a7c0c49371784ac5b28fb6cfcc
hoang <wafcolor@gmail.com>
2021-12-18 10:11:42 +0000

    fix undeclared identifier

--- fs/eventpoll.c
+++ fs/eventpoll.c
@@ -1263,27 +1263,13 @@ static int ep_create_wakeup_source(struct epitem *epi)
 {
 	struct name_snapshot n;
 	struct wakeup_source *ws;
-#ifdef CONFIG_LGE_PM
-	char buf[40] = {0,};
 
-	name = epi->ffd.file->f_path.dentry->d_name.name;
-	if (!name)
-		sprintf(buf, "eventpoll");
-	else
-		sprintf(buf, "eventpoll-%s", name);
-
-	if (!epi->ep->ws) {
-		epi->ep->ws = wakeup_source_register(buf);
-		if (!epi->ep->ws)
-			return -ENOMEM;
-	}
-#else
 	if (!epi->ep->ws) {
 		epi->ep->ws = wakeup_source_register("eventpoll");
 		if (!epi->ep->ws)
 			return -ENOMEM;
 	}
-#endif
+
 	take_dentry_name_snapshot(&n, epi->ffd.file->f_path.dentry);
 	ws = wakeup_source_register(n.name);
 	release_dentry_name_snapshot(&n);
